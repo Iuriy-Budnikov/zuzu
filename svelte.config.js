242afd76314295d380266faa9782f3e5aaeb3340
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2022-02-22 13:46:10 +0200

    add test blog

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,28 @@
 import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import preprocess from 'svelte-preprocess';
+import mdsvexConfig from './mdsvex.config.js';
 
 const dev = process.env.NODE_ENV === 'development';
 const filePath = dirname(fileURLToPath(import.meta.url));
 const sassPath = `${filePath}/src/lib/styles/`;
 
 const config = {
-  preprocess: preprocess({
-    scss: {
-      prependData: `
+  extensions: ['.svelte', '.md'],
+
+  preprocess: [
+    mdsvex(mdsvexConfig),
+    preprocess({
+      scss: {
+        prependData: `
 				@import '${sassPath}mixins/index.scss';
 				@import '${sassPath}variables/index.scss';
 			`
-    }
-  }),
+      }
+    })
+  ],
   kit: {
     paths: {
       base: ''
