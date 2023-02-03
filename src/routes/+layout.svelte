17cf26add590eaef4dc154dedf5089df451313a3
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2023-02-03 14:48:42 +0200

    add sentry replays

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,17 +28,17 @@
         tracesSampleRate: 1.0
       });
 
-      Sentry.init({
-        environment: env.PUBLIC_SENTRY_ENVIRONMENT,
-        dsn: env.PUBLIC_SENTRY_DSN,
-        integrations: [new Sentry.Replay()],
-        // This sets the sample rate to be 10%. You may want this to be 100% while
-        // in development and sample at a lower rate in production
-        replaysSessionSampleRate: 1.0,
-        // If the entire session is not sampled, use the below sample rate to sample
-        // sessions when an error occurs.
-        replaysOnErrorSampleRate: 1.0
-      });
+      // Sentry.init({
+      //   environment: env.PUBLIC_SENTRY_ENVIRONMENT,
+      //   dsn: env.PUBLIC_SENTRY_DSN,
+      //   integrations: [new Sentry.Replay()],
+      //   // This sets the sample rate to be 10%. You may want this to be 100% while
+      //   // in development and sample at a lower rate in production
+      //   replaysSessionSampleRate: 1.0,
+      //   // If the entire session is not sampled, use the below sample rate to sample
+      //   // sessions when an error occurs.
+      //   replaysOnErrorSampleRate: 1.0
+      // });
     }
   });
 </script>
