17d0cc11c047ba8181f83f25b9e428ac80308b40
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2023-02-03 13:38:36 +0200

    add sentry

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,35 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
-console.log('process.env.SENTRY_AUTH_TOKEN', process.env.SENTRY_AUTH_TOKEN);
-
 /** @type {import('vite').UserConfig} */
 const config = {
   build: {
     sourcemap: true // Source map generation must be turned on
   },
-  plugins: [
-    sveltekit(),
-    // Put the Sentry vite plugin after all other plugins
-    // sentryVitePlugin({
-    //   org: 'example-org',
-    //   project: 'example-project',
-
-    //   // Specify the directory containing build artifacts
-    //   include: './dist',
-
-    //   // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-    //   // and need `project:releases` and `org:read` scopes
-    //   authToken: process.env.SENTRY_AUTH_TOKEN
-
-    //   // Optionally uncomment the line below to override automatic release name detection
-    //   // release: process.env.RELEASE,
-    // })
-  ]
+  plugins: [sveltekit()]
 };
+
+if (
+  process.env.SENTRY_ORG_NAME &&
+  process.env.SENTRY_PROJECT_NAME &&
+  process.env.SENTRY_AUTH_TOKEN
+) {
+  config.push(
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG_NAME,
+      project: process.env.SENTRY_PROJECT_NAME,
+
+      // Specify the directory containing build artifacts
+      include: './.svelte-kit',
+
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need `project:releases` and `org:read` scopes
+      authToken: process.env.SENTRY_AUTH_TOKEN
+
+      // Optionally uncomment the line below to override automatic release name detection
+      // release: process.env.RELEASE,
+    })
+  );
+}
 
 export default config;
