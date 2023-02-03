506c317137db2db47973c33941c4a8b2e3515cc7
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2023-02-03 14:08:36 +0200

    add sentry replays

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,13 +19,25 @@
     if (!dev) {
       Sentry.init({
         environment: env.PUBLIC_SENTRY_ENVIRONMENT,
+
+        // DEFAULT
         dsn: env.PUBLIC_SENTRY_DSN,
         integrations: [new BrowserTracing()],
 
         // Set tracesSampleRate to 1.0 to capture 100%
         // of transactions for performance monitoring.
         // We recommend adjusting this value in production
-        tracesSampleRate: 1.0
+        tracesSampleRate: 1.0,
+
+        // REPLAY
+        // This sets the sample rate to be 10%. You may want this to be 100% while
+        // in development and sample at a lower rate in production
+        replaysSessionSampleRate: 0.1,
+        // If the entire session is not sampled, use the below sample rate to sample
+        // sessions when an error occurs.
+        replaysOnErrorSampleRate: 1.0,
+
+        integrations: [new Sentry.Replay()]
       });
     }
   });
